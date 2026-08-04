87165ea5d189b5e34c5375209e50519692ef57b1
kumao <okuma01205@gmail.com>
2026-08-04 22:23:31 +0900

    StudyRecordクラスを作成

---
+++ src/main/java/com/example/StudyRecord.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class StudyRecord {
+    private LocalDate studyDate;
+    private String subject;
+    private int studyMinutes;
+    private String memo;
+
+}
